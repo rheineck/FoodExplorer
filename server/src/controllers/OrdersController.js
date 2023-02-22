9101055a4986eec733de965fce2f54e58fe5cc3b
Raphael <raphaelheineck@gmail.com>
2023-02-22 16:49:51 -0300

    create method in order controller

--- server/src/controllers/OrdersController.js
+++ server/src/controllers/OrdersController.js
@@ -3,14 +3,14 @@ const AppError = require('../utils/AppError')
 
 class OrdersController {
   async create(req, res) {
-    const { id, quantity, user_id } = req.query
+    const { dishes_id, quantity, user_id } = req.query
     const { status } = req.body
     let i
-
-    const dishes_id = id.split(',')
+    
+    const dishesId = dishes_id.split(',')
     const dishesQuantity = quantity.split(',')
 
-    if(dishes_id.length !== dishesQuantity.length) {
+    if(dishesId.length !== dishesQuantity.length) {
       throw new AppError('Quantidade de pratos inconsistente! Verifique!')
     }
 
@@ -18,27 +18,32 @@ class OrdersController {
     let price = []
     let total = 0
 
-    for(i = 0; i < dishes_id.length; i++) {
-      [price[i]] = await knex('dishes').where({ id: dishes_id[i] }).select('price');
-      [dishes[i]] = await knex('dishes').where({ id: dishes_id[i] }).select('name')
+    for(i = 0; i < dishesId.length; i++) {
+      [price[i]] = await knex('dishes').where({ id: dishesId[i] }).select('price');
+      [dishes[i]] = await knex('dishes').where({ id: dishesId[i] }).select('name')
     }
 
-    for (i = 0; i < dishes_id.length; i++) {
-      total = total + price[i].price
+    for (i = 0; i < dishesId.length; i++) {
+      total = total + price[i].price*dishesQuantity[i]
     }
 
     total = total.toFixed(2)
     
-    await knex('orders').insert({
-      user_id,
-      dishes_id,
-      quantity: dishesQuantity,
-      total,
-      status
-    })
-    
-       
+    for (i = 0; i < dishesId.length; i++) {
+      await knex('orders').insert({
+        dishes_id: dishesId[i],
+        quantity: dishesQuantity[i],
+        user_id,
+        total,
+        status
+      })
+    }
+      
     return res.json()
+  }
+
+  async show(req,res) {
+    
   }
 }
 
