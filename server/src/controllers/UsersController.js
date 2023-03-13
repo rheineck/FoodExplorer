cd82c1f6ffc5dea5ed9653201b68c1227e95cf85
Raphael <raphaelheineck@gmail.com>
2023-03-13 16:19:19 -0300

    added user test with jest

--- server/src/controllers/UsersController.js
+++ server/src/controllers/UsersController.js
@@ -1,25 +1,17 @@
-  const { hash, compare } = require('bcryptjs')
+const { hash, compare } = require('bcryptjs')
 const AppError = require('../utils/AppError')
 const knex = require('../database/index')
+const UserRepository = require('../repositories/UserRepository')
+const UserCreateService = require('../services/UserCreateService')
 
 class UsersController {
   async create(req, res) {
     const { name, email, password, isAdmin } = req.body
 
-    const [checkUserExists] = await knex('users').select().where({email})
+    const userRepository = new UserRepository()
+    const userCreateService = new UserCreateService(userRepository)
 
-    if (checkUserExists) {
-      throw new AppError('Este email já está em uso!')
-    }
-
-    const hashedPassword = await hash(password, 8)
-
-    await knex('users').insert({
-      name,
-      email,
-      password: hashedPassword,
-      isAdmin
-    })
+    await userCreateService.execute({ name, email, password, isAdmin })
 
     return res.status(201).json()
 
