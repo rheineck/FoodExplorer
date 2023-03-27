ac1a41cec439746bbd00c664a69f41e746672bdd
Raphael <raphaelheineck@gmail.com>
2023-03-27 15:55:13 -0300

    Front-End Begin

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import GlobalStyles from './styles/global'
+import { ThemeProvider } from 'styled-components'
+
+import theme from './styles/theme'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+    <GlobalStyles />
+    </ThemeProvider>
   </React.StrictMode>,
 )
