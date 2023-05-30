de695a14c6b5a8f6316396b3bf0a4d5aafd2630b
raphael <raphaelheineck@gmail.com>
2023-05-30 17:31:14 -0300

    change in api.js

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: "http://localhost:3334"
+  baseURL: "https://foodexplorer-server.onrender.com"
 })
 
 // For production use: https://foodexplorer-server.onrender.com
